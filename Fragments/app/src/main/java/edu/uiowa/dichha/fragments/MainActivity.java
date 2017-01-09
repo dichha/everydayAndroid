4d200441fc7efb3e003cdc93e099adae5582f9fb
Dichha Rai <dichha.chamling@gmail.com>
2017-01-09 17:39:39 -0600

    ArticleFragment ClassCastException

--- Fragments/app/src/main/java/edu/uiowa/dichha/fragments/MainActivity.java
+++ Fragments/app/src/main/java/edu/uiowa/dichha/fragments/MainActivity.java
@@ -1,16 +1,17 @@
 package edu.uiowa.dichha.fragments;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements HeadlinesFragment.OnHeadlineSelectedListener{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_articles);
-        //Check that the activity is using the layout version with
+        //Check whether the activity is using the layout version with
         //the fragment_container FrameLayout
         if (findViewById(R.id.fragment_container) != null){
 
@@ -20,15 +21,16 @@ public class MainActivity extends AppCompatActivity {
             if(savedInstanceState != null){
                 return;
             }
+
             //Create a new Fragment to be placed in the activity layout
             HeadlinesFragment firstFragment = new HeadlinesFragment();
+
             //In case this activity was started with special instructions from an
             //Intent, pass the Intent's extras to the fragment as arguments
-
             firstFragment.setArguments(getIntent().getExtras());
 
             // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+            getSupportFragmentManager().beginTransaction();//.add(R.id.fragment_container,firstFragment).commit();
         }
     }
     public void onArticleSelected(int position){
